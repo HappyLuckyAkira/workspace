1d18a45327c11f1e8d5507e64f5f2a19bbcf4b4f
Akira Kubo <a_kubo99@yahoo.co.jp>
2024-09-07 13:23:38 +0000

    1st Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <gtest/gtest.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello world\n");
-  return 0;
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
+
+//int main(int argc, char *argv[]) {
+//  printf("Hello world\n");
+//  return 0;
+//}
